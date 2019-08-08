848af6359b1c1f398e07635cb132f8fc0bc289a5
jasonwu513 <jasonwu513@gmail.com>
2019-08-08 23:39:00 +0800

    use route -> controller -> view   create input view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+    <title>shortUrl</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Shorten your URL.</h1>
+        <form action="" method="post">
+            <input type="url" name="url" placeholder="ENTER your URL" >
+            <input type="submit" value="Shorten">
+        </form>
+    </div>
+</body>
+</html>
